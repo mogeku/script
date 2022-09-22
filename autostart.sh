6b20959314dfdce910a390d21389d6d2644e19ba
momo <1209816754@qq.com>
2022-09-22 21:19:00 +0800

    add vol and tochpad script

--- autostart.sh
+++ autostart.sh
@@ -2,6 +2,9 @@
 
 /bin/bash ~/script/dwm-status-refresh.sh &
 /bin/bash ~/script/wp-autochange.sh &
+/bin/bash ~/script/tap-to-click.sh &
+/bin/bash ~/script/inverse-scroll.sh &
 
 utools &
 nm-applet &
+cfw &
---
+++ vol-toggle.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+/usr/bin/amixer set Master toggle
+bash ~/script/dwm-status.sh
